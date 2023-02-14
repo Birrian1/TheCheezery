a8df5ee295e7b3a91870fc7269f24dc92687d504
DESKTOP-UCNGISC\BCJL1 <91810728+Birrian1@users.noreply.github.com>
2023-02-13 22:35:46 -0700

    Productos

--- app/src/main/java/brian/jimenez/thecheezery/MenuActivity.kt
+++ app/src/main/java/brian/jimenez/thecheezery/MenuActivity.kt
@@ -1,11 +1,50 @@
 package brian.jimenez.thecheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+
+        btnCold.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+        var btnHot: Button = findViewById(R.id.button_hot_drinks) as Button
+
+        btnHot.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosHot::class.java)
+            startActivity(intent)
+        }
+
+
+        var btnSweet: Button = findViewById(R.id.button_sweet) as Button
+
+        btnSweet.setOnClickListener {
+            var intent: Intent = Intent(this, ProductoSweet::class.java)
+            startActivity(intent)
+        }
+
+
+
+        var btnSalties: Button = findViewById(R.id.button_salties) as Button
+
+        btnSalties.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosSalties::class.java)
+            startActivity(intent)
+        }
+
+
+
+
     }
 }
---
+++ app/src/main/java/brian/jimenez/thecheezery/ProductosHot.kt
@@ -0,0 +1,99 @@
+package brian.jimenez.thecheezery
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
+
+class ProductosHot : AppCompatActivity() {
+
+
+    var hotDrinks = ArrayList<Product>()
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_productos_hot)
+
+
+        var listView: ListView = findViewById(R.id.litviewhot) as ListView
+
+        agregarProducto()
+
+        var adaptador: AdaptadorProductos = AdaptadorProductos(this, hotDrinks)
+        listView.adapter = adaptador
+
+
+
+
+    }
+
+
+    fun agregarProducto(){
+        hotDrinks.add(Product("Latte", R.drawable.latte, "Coffee drink made with espresso and steamed milk", 6.0))
+        hotDrinks.add(Product("Hot chocolate", R.drawable.hotchocolate, "Heated drink consisting of shaved chocolate, topped with marshmallows.", 5.0))
+        hotDrinks.add(Product("Espresso", R.drawable.espresso, "Full-flavored, concentrated form of coffee.", 4.0))
+        hotDrinks.add(Product("Chai Latte", R.drawable.chailatte, "Spiced tea concentrate with milk", 6.0))
+        hotDrinks.add(Product("Capuccino", R.drawable.capuccino, "A cappuccino is an espresso-based coffee drink, prepared with steamed foam.", 7.0))
+        hotDrinks.add(Product("American coffee", R.drawable.americano, "Espresso with hot water", 2.0))
+
+    }
+
+
+    private class AdaptadorProductos: BaseAdapter {
+        var productos = ArrayList<Product>()
+        var contexto: Context? = null
+
+        constructor(contexto: Context, productos: ArrayList<Product>){
+            this.contexto = contexto
+            this.productos = productos
+        }
+
+        override fun getCount(): Int {
+            return productos.size
+        }
+
+        override fun getItem(p0: Int): Any {
+            return productos[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+
+            var prod = productos[p0]
+            var inflador = LayoutInflater.from(contexto)
+            var vista = inflador.inflate(R.layout.producto_view, null)
+
+            var imagen = vista.findViewById(R.id.producto_img) as ImageView
+            var nombre = vista.findViewById(R.id.producto_nombre) as TextView
+            var desc = vista.findViewById(R.id.producto_desc) as TextView
+            var precio = vista.findViewById(R.id.producto_precio) as TextView
+
+            imagen.setImageResource(prod.image)
+            nombre.setText(prod.name)
+            desc.setText(prod.description)
+            precio.setText("$${prod.price}")
+
+            return vista
+
+        }
+
+
+    }
+
+
+
+
+
+
+}
